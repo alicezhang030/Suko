9ca46a5adcb77c48d3a8a952208c8a1d562e4f8e
Alice Zhang <jinyangzhangcontact@gmail.com>
2022-07-08 16:40:18 -0700

    Add navigation controller and table view to library VC

---
+++ Suko/View Controller/SUKLibraryViewController.h
@@ -0,0 +1,16 @@
+//
+//  SUKLibraryViewController.h
+//  Suko
+//
+//  Created by Alice Zhang on 7/8/22.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SUKLibraryViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Suko/View Controller/SUKLibraryViewController.m
@@ -0,0 +1,31 @@
+//
+//  SUKLibraryViewController.m
+//  Suko
+//
+//  Created by Alice Zhang on 7/8/22.
+//
+
+#import "SUKLibraryViewController.h"
+
+@interface SUKLibraryViewController ()
+
+@end
+
+@implementation SUKLibraryViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
